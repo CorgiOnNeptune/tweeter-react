ef40d333d5e6338e8390ba29cb3ea0d04e7bffdf
Justin <justinjdbennett@gmail.com>
2023-01-08 16:13:27 -0700

    Import HTML to components

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Navigation from './components/Navigation';
 import Profile from './components/Profile';
-import TweetList from './components/TweetList';
 import TweetForm from './components/TweetForm';
+import TweetList from './components/TweetList';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Profile />
-      <TweetList />
-      <TweetForm />
+      <main class="container">
+        <TweetList />
+        <TweetForm />
+      </main>
     </div>
   );
 }
